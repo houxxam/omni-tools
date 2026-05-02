a8c5074fbd790cc8c0c75a1970facf886920bbcc
houxxam <houxxam95@gmail.com>
2026-05-02 19:02:09 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,26 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  base: '/omni-tools/',
+
   plugins: [react(), tsconfigPaths()],
+
   define: {
     'process.env': {}
   },
+
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
   },
+
   test: {
     globals: true,
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
     include: ['**/*.test.{ts,tsx}']
   },
-  worker: { format: 'es' }
+
+  worker: {
+    format: 'es'
+  }
 });
